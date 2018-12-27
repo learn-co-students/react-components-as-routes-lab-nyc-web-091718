f24ade2500695d047aaba29a647059858a84b101
asc5025 <albert.s.chun@gmail.com>
2018-12-27 17:56:46 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, idx) => (
+        <div key={idx}>
+          <p>Name: {actor.name}</p>
+          <p>Movies:</p>
+          <ul>
+            {actor.movies.map((m, idx) => (
+              <li key={idx}>{m}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
